51ff241b21adf7d9a5a36d5f6d7eff7dc83d9084
GunnarSupercar <cutuoff2@hotmail.com>
2020-09-24 11:48:41 -0400

    Adjusted critical hit chance, buffed Focus Energy

--- data/battle/critical_hit_chances.asm
+++ data/battle/critical_hit_chances.asm
@@ -1,8 +1,8 @@
 CriticalHitChances:
-	db 1 out_of 15 ;  0
+	db 1 out_of 16 ;  0 - This was originally 1/15, which isn't accurate to what's listed on Bulbapedia. I prefer this amount anyway, just making a note here.
 	db 1 out_of 8  ; +1
 	db 1 out_of 4  ; +2
-	db 1 out_of 3  ; +3
-	db 1 out_of 2  ; +4
-	db 1 out_of 2  ; +5
-	db 1 out_of 2  ; +6
+	db 1 out_of 2  ; +3 - Increased from 1/3.
+	db 1 out_of 1  ; +4 - Increased from 1/2.
+	db 1 out_of 1  ; +5 - Increased from 1/2.
+	db 1 out_of 1  ; +6 - Increased from 1/2.
